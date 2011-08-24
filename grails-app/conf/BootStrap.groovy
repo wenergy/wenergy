e618b28700be03593d34d59c0dd0783e30fbe236
ddauer <ddauer@gmail.com>
2011-08-24 19:20:53 +0200

    Fix BootStrap code
    app startup failed because of duplicate objects

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -12,14 +12,18 @@ class BootStrap {
     def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true)
     def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
 
-    Peergroup peergroup = new Peergroup(name: "homies")
-    peergroup.save()
-    Household h = new Household(macAddress: "de:ad:be:ef:fe:ed", firstName: "arduino1", lastName: "dalen",  eMail: "dalen@kit.edu", address: "address",
-        username: "id1", password: springSecurityService.encodePassword('password'), enabled: true)
-    h.addToPeergroups(peergroup)
-    h.save()
-    if (!h.authorities.contains(adminRole)) {
-      HouseholdRole.create h, adminRole
+    def macAddress = "de:ad:be:ef:fe:ed"
+    Household household = Household.findByMacAddress(macAddress)
+    // Don't create household again on tomcat reboot or redeploy
+    if (!household) {
+      Peergroup peergroup = Peergroup.findByName("homies") ?: new Peergroup(name: "homies").save(failOnError: true)
+      household = new Household(macAddress: macAddress, firstName: "arduino1", lastName: "dalen", eMail: "dalen@kit.edu", address: "address",
+          username: "id1", password: springSecurityService.encodePassword('password'), enabled: true)
+      household.addToPeergroups(peergroup)
+      household.save()
+      if (!household.authorities.contains(adminRole)) {
+        HouseholdRole.create household, adminRole
+      }
     }
 
 /*        int i = 1
@@ -42,14 +46,14 @@ class BootStrap {
 
     /* Debug consumptions */
 //    int j = 0
-//    150.times {
-//
-//      Consumption consumption = new Consumption(macAddress: "de:ad:be:ef:fe:ed", powerReactive: new BigDecimal(j), powerReal: new BigDecimal(j), timestamp: new Date(j as long))
-//      h.addToConsumptions(consumption)
-//
-//      j++
-//    }
-//    h.save()
+    //    150.times {
+    //
+    //      Consumption consumption = new Consumption(macAddress: macAddress, powerReactive: new BigDecimal(j), powerReal: new BigDecimal(j), timestamp: new Date(j as long))
+    //      household.addToConsumptions(consumption)
+    //
+    //      j++
+    //    }
+    //    household.save()
 
   }
   def destroy = {
