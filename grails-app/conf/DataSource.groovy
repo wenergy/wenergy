64fa594f5e0fa3ed6e747ef2510cfd2c03c3f0d2
ddauer <ddauer@gmail.com>
2011-08-24 16:56:18 +0200

    Add MySQL Production Connection

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,32 +1,44 @@
 dataSource {
-    pooled = true
-    driverClassName = "org.hsqldb.jdbcDriver"
-    username = "sa"
-    password = ""
+  pooled = true
+  driverClassName = "org.hsqldb.jdbcDriver"
+  username = "sa"
+  password = ""
 }
 hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = true
-    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+  cache.use_second_level_cache = true
+  cache.use_query_cache = true
+  cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
 }
 // environment specific settings
 environments {
-    development {
-        dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-            url = "jdbc:hsqldb:mem:devDB"
-        }
+  development {
+    dataSource {
+      dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+      url = "jdbc:hsqldb:mem:devDB"
     }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:hsqldb:mem:testDb"
-        }
+  }
+  test {
+    dataSource {
+      dbCreate = "update"
+      url = "jdbc:hsqldb:mem:testDb"
     }
-    production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:hsqldb:file:prodDb;shutdown=true"
-        }
+  }
+//    production {
+  //        dataSource {
+  //            dbCreate = "update"
+  //            url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+  //        }
+  //    }
+  production {
+    dataSource {
+      pooled = true
+      driverClassName = "com.mysql.jdbc.Driver"
+      dbCreate = "update" // one of 'create', 'create-drop','update'
+      username = "wattsoever"
+      password = "Vs4QYp2uaF"
+      url = "jdbc:mysql://localhost/wattsoever_prod"
+      dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
     }
+  }
+
 }
