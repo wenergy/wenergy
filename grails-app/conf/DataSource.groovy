757b5a883860713f4ace11447a6f803c1c88d676
ddauer <ddauer@gmail.com>
2011-10-10 23:44:43 +0200

    Update DataSource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,43 +1,46 @@
 dataSource {
-    pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+  pooled = true
+  driverClassName = "org.h2.Driver"
+  username = "sa"
+  password = ""
 }
 hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = true
-    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+  cache.use_second_level_cache = true
+  cache.use_query_cache = true
+  cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
 }
 // environment specific settings
 environments {
-    development {
-        dataSource {
-            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:prodDb"
-        }
+  development {
+    dataSource {
+      dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+      url = "jdbc:h2:prodDb"
     }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb"
-        }
+  }
+  test {
+    dataSource {
+      dbCreate = "update"
+      url = "jdbc:h2:mem:testDb"
     }
-    production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb"
-            // For MySQL production scenarios enable the following settings
-//          pooled = true
-//          properties {
-//               minEvictableIdleTimeMillis=1800000
-//               timeBetweenEvictionRunsMillis=1800000
-//               numTestsPerEvictionRun=3
-//               testOnBorrow=true
-//               testWhileIdle=true
-//               testOnReturn=true
-//               validationQuery="SELECT 1"
-//          }
-        }
+  }
+  production {
+    dataSource {
+      dbCreate = "update"
+      url = "jdbc:mysql://localhost/wattsoever_prod?useUnicode=true&characterEncoding=UTF-8"
+      driverClassName = "com.mysql.jdbc.Driver"
+      dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+      username = "wattsoever"
+      password = "Vs4QYp2uaF"
+      pooled = true
+      properties {
+        minEvictableIdleTimeMillis = 1800000
+        timeBetweenEvictionRunsMillis = 1800000
+        numTestsPerEvictionRun = 3
+        testOnBorrow = true
+        testWhileIdle = true
+        testOnReturn = true
+        validationQuery = "SELECT 1"
+      }
     }
+  }
 }
