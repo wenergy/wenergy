374194096917f94391bdf51b05e9ccf34b0450da
ddauer <ddauer@gmail.com>
2012-02-24 09:07:10 +0100

    Reformatting

--- grails-app/jobs/edu/kit/im/HouseholdJob.groovy
+++ grails-app/jobs/edu/kit/im/HouseholdJob.groovy
@@ -2,16 +2,17 @@ package edu.kit.im
 
 class HouseholdJob {
 
-    static triggers = {
-        // "0 0 0 * * ?" // every day at midnight
-        // "0 0/15 6-22 * * ?" // every 15 minutes between 6am-11pm
-        // '0 * * * * ?' // every minute (test)
-        cron cronExpression: "0 0 3 * * ?" // wEnergy Scheduler, every day at 3am
-    }
-    def householdService
-    def execute() {
-      householdService.run()
+  static triggers = {
+    // "0 0 0 * * ?" // every day at midnight
+    // "0 0/15 6-22 * * ?" // every 15 minutes between 6am-11pm
+    // '0 * * * * ?' // every minute (test)
+    cron cronExpression: "0 0 3 * * ?" // wEnergy Scheduler, every day at 3am
+  }
+  def householdService
+
+  def execute() {
+    householdService.run()
 //    log.error "execute"
-    }
+  }
 
 }
