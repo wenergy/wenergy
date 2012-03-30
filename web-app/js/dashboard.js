13de73946c9f7dadf0e21f2e77747e606888373b
ddauer <ddauer@gmail.com>
2012-03-30 11:52:47 +0200

    Logarithmic axis type

--- web-app/js/dashboard.js
+++ web-app/js/dashboard.js
@@ -120,6 +120,7 @@ $(function () {
     if (numberOfValues !== cache.numberOfValues || cache.initialLoading) {
       // Update cache
       cache.numberOfValues = numberOfValues;
+      cache.axisType = axisType;
       // Force reload of all data
       cache.deltaDate = 0;
 
@@ -130,12 +131,13 @@ $(function () {
     } else {
       // We get here only if axisType has changed, therefore no reloading is necessary
 
-      // Update graph
+      // Update chart
       if (axisType != cache.axisType) {
         // Update cache
         cache.axisType = axisType;
 
-        // TODO: destroy+create
+        // Update chart
+        updateChart(true);
       }
     }
   });
@@ -310,12 +312,15 @@ $(function () {
     });
   }
 
-  function updateChart() {
+  function updateChart(reload) {
+    // Default parameters
+    reload = typeof reload !== 'undefined' ? reload : false;
+
     // Get all values from cache
     var cache = $("#dashboard").data("bbq");
 
     // Delta updates don't need a chart update
-    if (cache.isDelta) {
+    if (cache.isDelta && !reload) {
       return;
     }
 
@@ -355,7 +360,9 @@ $(function () {
             return this.value + ' W';
           }
         },
-        minorTickInterval:'auto'
+        minorTickInterval:'auto',
+        type:cache.axisType,
+        min:(cache.axisType == 'logarithmic' ? 10 : null)
       },
 
       tooltip:{
@@ -412,6 +419,10 @@ $(function () {
       ]
     };
 
+    if (reload && cache.consumptionChart != null) {
+      cache.consumptionChart.destroy();
+    }
+
     cache.consumptionChart = new Highcharts.Chart(consumptionChartOptions);
   }
 
@@ -456,111 +467,4 @@ $(function () {
 
   // Final step is to trigger the hash change event which will also handle initial data loading
   $(window).trigger("hashchange");
-
-
-  function ___XXXXreloadData() {
-    var horizon;
-    var horizonLength;
-    horizon = $("#optionsForm input[name='horizon']:checked").val();
-    horizonLength = parseInt(horizon);
-    $.ajax({
-      type:"POST",
-      url:rootPath + "data/dashboardData",
-      data:{
-        horizon:horizonLength
-      },
-      success:function (json) {
-        // Reset data and extract new values from json
-        phase1Data = [];
-        phase2Data = [];
-        phase3Data = [];
-        currentLevel = 0;
-        // Make sure data really exists to avoid "undefined" errors
-        if (json.data) {
-          phase1Data = json.data.powerPhase1
-          phase2Data = json.data.powerPhase2
-          phase3Data = json.data.powerPhase3
-          currentLevel = json.data.currentLevel
-        }
-
-        // Plot
-        var optionsConsumption = {
-          series:{
-            // Valid for all data sets
-            stack:0,
-            lines:{
-
-              show:true,
-              fill:0.85,
-              lineWidth:0.5,
-              steps:true
-            },
-            bars:{ show:false }
-          },
-          colors:["#c00000", "#bf8686", "#d2d2d2"],
-          xaxis:{
-            show:false,
-            min:0,
-            max:horizonLength - 1
-          },
-          yaxis:{
-            min:0,
-            tickFormatter:powerFormatter
-          },
-          grid:{
-            borderWidth:1.0
-          },
-          legend:{
-            show:true,
-            container:"#consumptionGraphLegend"
-          }
-        }
-
-        var optionsLevel = {
-          series:{
-            // Valid for all data sets
-            lines:{
-              show:true,
-              fill:0.75,
-              fillColor:{colors:[ "#7CC718", "#F2F962", "#990000"]},
-              lineWidth:0.25,
-              steps:true
-            }
-          },
-          yaxis:{
-            show:false,
-            //position:"right",
-            min:0,
-            max:1.0
-          },
-          xaxis:{
-            show:false,
-            min:0,
-            max:1
-          },
-          grid:{
-
-            borderWidth:1.0
-          }
-        }
-
-        var flotPhase1Series = [];
-        var flotPhase2Series = [];
-        var flotPhase3Series = [];
-        var dataLevelGraph = [];
-
-        flotPhase1Series.push({ label:"Phase 1", data:phase1Data, color:"#990000"});
-        flotPhase2Series.push({ label:"Phase 2", data:phase2Data, color:"#009900"});
-        flotPhase3Series.push({ label:"Phase 3", data:phase3Data, color:"#000099"});
-        dataLevelGraph.push({ data:currentLevel, color:"#333333"});
-
-        $.plot($("#consumptionGraph"), [phase1Data, phase2Data, phase3Data], optionsConsumption);
-        $.plot($("#levelGraph"), dataLevelGraph, optionsLevel);
-        // TODO: optimize drawing (clean param)
-      }
-
-    });
-
-
-  }
 });
