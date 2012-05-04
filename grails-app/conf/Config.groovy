9ce6c85a4fa40cbeb37c12cf35949c17786af574
ddauer <ddauer@gmail.com>
2012-05-04 14:13:54 +0200

    Disable tracking

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -138,6 +138,7 @@ grails.plugins.springsecurity.interceptUrlMap = [
     '/imprint/**': ["permitAll"],
     '/registration/**': ["permitAll"],
     '/data/welcome': ["permitAll"],
+    '/data/event': ["permitAll"],
     // User profile
     '/user/**': ["hasRole('ROLE_USER')"],
     // Block controllers
