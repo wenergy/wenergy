bac775329f11271093d4a8e00254b9649c25f12e
ddauer <ddauer@gmail.com>
2012-05-06 15:09:46 +0200

    Update backup script

--- vm-scripts/backup.sh
+++ vm-scripts/backup.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
 # wEnergy Backup Script
-# ---------------------
-# Instructions:
-# 1. Install XtraBackup (http://www.percona.com/doc/percona-xtrabackup/index.html)
-# 2. Install crontab (sudo crontab -e):
-# @daily /home/cf/backup.sh > /home/cf/backup.log
+# Install crontab (sudo crontab -e):
+# "@daily /home/cf/backup.sh >/home/cf/backup.log 2>&1"
+# Alternatively run using manually sudo
 
 BACKUP_ROOT="/home/cf/backup"
+BACKUP_TEMP="/home/cf/backup_temp"
 BACKUP_MYSQL="$BACKUP_ROOT/mysql"
 BACKUP_VCAP="$BACKUP_ROOT/vcap"
-BACKUP_DATE=`date +%Y%m%d%H%M%S`
+BACKUP_DATE=$(date +%Y-%m-%d_%H-%M-%S)
+BACKUP_LOG_FILE="/tmp/innobackupex_stderr"
 
 # Backup mysql
-innobackupex --defaults-file=/etc/mysql/my.cnf --user=root --password=mysql --use-memory=1G --stream=tar ./ | gzip -c -9 > $BACKUP_MYSQL/mysql.$BACKUP_DATE.tar.gz
+echo "$BACKUP_DATE:\tBacking up mysql"
+innobackupex --defaults-file=/etc/mysql/my.cnf --user=root --password=mysql --use-memory=1G --stream=xbstream --compress $BACKUP_TEMP >$BACKUP_MYSQL/mysql.$BACKUP_DATE.xbstream 2>$BACKUP_LOG_FILE
+cat $BACKUP_LOG_FILE
+rm $BACKUP_LOG_FILE
 
 # Backup vcap config
+echo "$BACKUP_DATE:\tBacking up vcap config"
 tar -czpf $BACKUP_VCAP/vcap.$BACKUP_DATE.tar.gz /home/cf/cloudfoundry/.deployments/devbox/config
 
 # Remove backups older than 5 days
