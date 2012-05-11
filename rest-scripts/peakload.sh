a27405517ee12f095a5904f1170b0521c938cb88
ddauer <ddauer@gmail.com>
2012-05-11 12:24:56 +0200

    Accept huseholdId parameter for peaklaod script

--- rest-scripts/peakload.sh
+++ rest-scripts/peakload.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-
+if [ -z $1 ]
+then
+	uid=1
+else
+	uid=$1
+fi
 i=1
 m=10
 n=15
@@ -15,7 +20,7 @@ p1=$(expr $i \* $m)
 p2=$(expr $i \* $n)
 p3=$(expr $i \* $o)
 b=$(expr $p - $i)
-result=$(curl -g -o /dev/null -s -w %{time_connect}:%{time_starttransfer}:%{time_total} http://$server/api/consumption?json={\"id\":1\,\"p1\":$p1\,\"p2\":$p2\,\"p3\":$p3\,\"b\":$b})
+result=$(curl -g -o /dev/null -s -w %{time_connect}:%{time_starttransfer}:%{time_total} http://$server/api/consumption?json={\"id\":$uid\,\"p1\":$p1\,\"p2\":$p2\,\"p3\":$p3\,\"b\":$b})
 stat=$(echo $result | gawk -F: '{ print $1" / "$2" / "$3}')
 echo $stat
 i=$(expr $i + 1)
